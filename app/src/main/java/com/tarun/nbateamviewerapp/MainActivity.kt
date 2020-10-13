240f39d3fbda592b5c35e5a5a4798fa47c89701c
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2020-10-13 16:28:27 -0700

    Initial commit

---
+++ app/src/main/java/com/tarun/nbateamviewerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tarun.nbateamviewerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
